bbf125ce5d32b4185fdd0d70c5852534cd560494
RaphaellArcanjo <raphaellarcanjo@proton.me>
2025-12-03 16:06:20 -0300

    feat: implement read_songs logic and add .gitignore

---
+++ Sources/song.c
@@ -0,0 +1,44 @@
+//
+// Created by raphaell on 03/12/2025.
+//
+
+#include "song.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int read_songs(char *file_name, Song* songs, int max_songs) {
+     FILE* fp = fopen(file_name, "r");
+
+
+
+     if (fp == NULL) {
+          return 0;
+     }
+
+     char line[100];
+     int i = 0; //Contador de musicas lidas
+
+
+     while (fgets(line, sizeof(line), fp) != NULL && i < max_songs) {
+
+
+          char *token = strtok(line, ";");
+          songs[i].id = (int)strtol(token, NULL, 10);
+
+          token = strtok(NULL, ";");
+          strcpy(songs[i].title, token);
+
+          token = strtok(NULL, ";");
+          strcpy(songs[i].artist, token);
+
+          token = strtok(NULL, ";");
+          songs[i].duration = (int)strtol(token, NULL, 10);
+
+          i++;
+     }
+     fclose(fp);
+
+     return i;
+}
